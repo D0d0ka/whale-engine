d784fdefab27b8a36957a68d3820e6aaf1585a02
dodo_k <kaur.aid@gmail.com>
2026-03-01 20:11:03 +0200

    gitignore fix

--- WhaleEngine.py
+++ WhaleEngine.py
@@ -10,13 +10,19 @@ presset = """from WhaleEngine import *
 
 app = WhaleEngine(title="Whale engine app")
 render = Renderer2D()
+#app.input = InputSystem()
 shapes = LoadShapes()
 
+#entity = Entity2D(texture=shapes.square)
+
 def update(dt):
+    #if app.input.key(glfw.KEY_SPACE):
+    #    entity.rotation += 90 * dt
+    #    entity.x += 100 * dt
     pass
 app.update = update
 
-app.run())"""
+app.run()"""
 
 # other stuff
 class rarity:
