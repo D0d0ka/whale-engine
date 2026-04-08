7f4f85a9fe7419a4c42256a00127f4e23587bb65
dodo_k <kaur.aid@gmail.com>
2026-04-08 17:36:41 +0300

    agent provider extension + logging

--- agentprovider.py
+++ agentprovider.py
@@ -1,9 +1,23 @@
 import os
+from WhaleEngine.logging import *
+
+set_logging_file("agent.log")
+
+allowed_extensions = [".py", ".png", ".mp3"]
+
+logLn("Agent started. Scanning WhaleEngine package for python files.", "agent")
+logLn("Allowed file extensions: " + ", ".join(allowed_extensions), "agent")
 
 folder = "WhaleEngine"
+logLn(f"Scanning folder: {folder}", "agent")
+
 with open("agent.txt", "w") as f:
     f.write("All python files in the WhaleEngine package code:\n")
     for root, dirs, files in os.walk(folder):
         for file in files:
-            if file.endswith(".py"):
-                f.write(os.path.join(root, file) + "\n")
+            for i in allowed_extensions:
+                if file.endswith(i):
+                    logLn(f"Found file: {os.path.join(root, file)}", "agent")
+                    f.write(os.path.join(root, file) + "\n")
+
+logLn("Agent finished scanning.", "agent")
