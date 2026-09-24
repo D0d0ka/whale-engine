5345b6a339dc1ce23a86275a9ff74daf651f3acb
dodo_k <kaur.aid@gmail.com>
2026-09-24 12:18:20 +0300

    bouncing v2

--- examples/bouncing.py
+++ examples/bouncing.py
@@ -3,34 +3,54 @@ from WhaleEngine.D2 import *
 from WhaleEngine.WindowAPI.OpenGL import windowAPI # or Vulkan / WebGL
 from WhaleEngine.helpers.fpscounter import *
 
-window = windowAPI(title="Bouncing", target_fps=float("inf"))
+window = windowAPI(title="Bouncing", target_fps=60)#float("inf"))
 window.set_color(Color.cyan)
 app = WhaleEngine(window=window)
 renderer = Renderer2D()
 app.input = InputSystem()
 shapes = LoadShapes()
 
-GRAVITY = 980
-LOSS_MULTIPLIER = 0.6
+GRAVITY = 1000
+LOSS_MULTIPLIER = 0.75
 START_HEIGHT = 300
 GROUND_Y = -300
+START_SPEED_Y = 1000
+ELASTICITY = 0.6
+START_SCALE_Y = 1
 
-pall = Entity2D(texture=shapes.circle, color=Color.red, position=(0, START_HEIGHT))
-maapind = Entity2D(texture=shapes.square, color=Color.green, scale=(10,1),position=(0,GROUND_Y))
+ball = Entity2D(texture=shapes.circle, color=Color.red, position=(0, START_HEIGHT), scale=(1, START_SCALE_Y))
+ground = Entity2D(texture=shapes.square, color=Color.green, scale=(50,1),position=(0,GROUND_Y))
 
-speed_y = 0
+#ball.scale_y = START_SCALE_Y*ELASTICITY
+
+speed_y = START_SPEED_Y
 
 def update(dt):
     global speed_y
     FPS_counter(dt)
     window.set_title(f"Bouncing - FPS: {round(get_FPS())}")
-    speed_y -= GRAVITY * dt
-    pall.y += speed_y * dt
-    if pall.y <= GROUND_Y+maapind.scale_y*100:
-        pall.y = GROUND_Y+maapind.scale_y*100
-        speed_y = -speed_y * LOSS_MULTIPLIER
     if app.input.key_pressed(Keys.ESCAPE):
-      app.exit()
+        app.exit()
+    groundpoint = GROUND_Y + ground.scale_y * 50 + ball.scale_y * 50
+    if ball.y <= groundpoint and abs(speed_y) < 200:
+        speed_y = 0
+        ball.y = groundpoint
+        ball.scale_y = START_SCALE_Y
+        return
+    speed_y -= GRAVITY * dt
+    if ball.scale_y < START_SCALE_Y:
+        ball.scale_y += 5 * dt
+        if ball.scale_y > START_SCALE_Y:
+            ball.scale_y = START_SCALE_Y
+    if speed_y < 0:
+        ball.y += speed_y * dt
+        if ball.y <= groundpoint:
+            ball.y = groundpoint
+            ball.scale_y = START_SCALE_Y * ELASTICITY
+            speed_y = -speed_y * LOSS_MULTIPLIER
+    else:
+        ball.y += speed_y * dt
+
 app.update = update
 
 def on_app_close():
